133db3d3e697bd01ca9d4678c6e7beea2cc8a771
kucingandromeda <agvayazem@gmail.com>
2024-11-08 16:08:48 +0700

    BUILD: found Asymmetric Encryption in openssl

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 mod block;
 mod chain;
 
+
 use block::block::Block;
+use openssl::{bn::BigNum, rsa};
 
 use crate::chain::chain::Chain;
 
 fn main(){
     let mut chain = Chain::new();
+
+    let a = BigNum::from_u32(2042).unwrap();
+    let b = BigNum::from_u32(2042).unwrap();
+    let c = BigNum::from_u32(2042).unwrap();
+    let a = rsa::RsaPrivateKeyBuilder::new(a,b,c).unwrap().build();
 
     let yazem_block = Block::new("yazem".to_string(), 10);
     chain.add_block(yazem_block);
